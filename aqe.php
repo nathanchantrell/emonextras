e9c08ffe34f815dfcd50aba9ea590dd97d52f9f9
Nathan Chantrell <nathan@chantrell.net>
2013-03-03 22:27:26 +0000

    Change aqe.php to use Cosm API

--- aqe.php
+++ aqe.php
@@ -1,30 +1,36 @@
 <?php
-// Emonextras - Scrape NO2 and CO values from airqualityegg.com and post to emoncms
+// Emonextras - Get AirQualityEgg values from Cosm.com JSON feed and post to emoncms
 // Run via cron with (for example): */5 * * * * php /path/to/aqe.php >/dev/null 2>&1
 // By Nathan Chantrell http://nathan.chantrell.net
 
-// Replace FEEDNO with feed number of Air Quality Egg
-$str = file_get_contents("http://airqualityegg.com/egg/FEEDNO");
+// Add your Cosm and Emoncms details below
+define ("COSMFEEDID", "XXXXXX"); // Cosm feed ID (Air Quality Egg feed)
+define ("COSMAPIKEY", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"); // Cosm API Key
+define ("EMONCMSAPIKEY", "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"); // Emoncms API Key
+define ("EMONCMSSERVER", "http://my.server/emoncms"); // Location of Emoncms server
 
-// NO2
-// Replace SERVER and APIKEY to reflect your emoncms installation
-$s = explode('<h3 class="current-id-full">Nitrogen Dioxide</h3>
-      <div class="current-value">
-        <p class="current-value-measure">
-          <span class="current-value-measure-wrap">',$str);
-$s = explode('</span>',$s[1]);
-if(!($s[0] == NULL)) {
- file_get_contents('http://SERVER/emoncms/input/post?json={no2:' . $s[0] . '}&apikey=APIKEY');
-}
+// You shouldn't need to change anything below this point
 
-// CO
-// Replace SERVER and APIKEY to reflect your emoncms installation
-$s = explode('<h3 class="current-id-full">Carbon Monoxide</h3>
-      <div class="current-value">
-        <p class="current-value-measure">
-          <span class="current-value-measure-wrap">',$str);
-$s = explode('</span>',$s[1]);
-if(!($s[0] == NULL)) {
- file_get_contents('http://SERVER/emoncms/input/post?json={co:' . $s[0] . '}&apikey=APIKEY');
-}
+// Get the JSON from Cosm
+$url = "https://api.cosm.com/v2/feeds/" . COSMFEEDID . ".json";
+
+$options = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"X-ApiKey: " . COSMAPIKEY . ""
+  )
+);
+
+$context = stream_context_create($options);
+$json_string = file_get_contents($url, false, $context);
+$parsed_json = json_decode($json_string,true);
+
+$co = $parsed_json[datastreams][0][current_value];
+$no2 = $parsed_json[datastreams][3][current_value];
+$humidity = $parsed_json[datastreams][2][current_value];
+$temperature = $parsed_json[datastreams][5][current_value];
+
+// Update emoncms
+file_get_contents(EMONCMSSERVER . '/input/post?json={aqe_co:' . $co . '},{aqe_no2:' . $no2 . '},{aqe_humidity:' . $humidity . '},{aqe_temp:' . $temperature . '}&apikey=' . EMONCMSAPIKEY);
+
 ?>
